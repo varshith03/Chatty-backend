c89bc37860d613d657539ebaa4a31d1e2f2e2799
varshith03 <svarshith.vs@gmail.com>
2024-03-31 18:49:23 +0530

    vercel file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 dotenv.config();
 connectDB();
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json()); //to accept json data
 
 // express js api
